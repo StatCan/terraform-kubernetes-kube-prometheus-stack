54b7a5922e636fbd2214abb31859b73a44ff5835
Frances Zsurka <frances.zsurka@cloud.statcan.ca>
2021-04-16 21:08:35 +0000

    feat: shared rules, clean: folders, fix: release name issues

--- variables.tf
+++ variables.tf
@@ -18,11 +18,19 @@ variable "helm_repository_username" {
   default     = ""
   description = "The username of the repository where the Helm chart is stored"
 }
+variable "chart_version" {
+  description = "Version of the Helm chart"
+}
 
 variable "enable_destinationrules" {
-  type        = string
-  default     = "0"
+  type        = bool
+  default     = false
   description = "Creates DestinationRules for Prometheus, Alertmanager, Grafana, and Node Exporters"
+}
+variable "destinationrules_prefix" {
+  type        = string
+  default     = "kube-prometheus-stack"
+  description = "Set to Prom/AM svc prefix if using non-default helm_release"
 }
 variable "destinationrules_mode" {
   type        = string
@@ -35,8 +43,15 @@ variable "cluster_domain" {
   description = "Cluster domain for DestinationRules"
 }
 
-variable "chart_version" {
-  description = "Version of the Helm chart"
+variable "enable_prometheusrules" {
+  type        = bool
+  default     = false
+  description = "Adds Prometheus Rules for general platform alerts"
+}
+variable "prometheus_pvc_name" {
+  type        = string
+  default     = "prometheus-kube-prometheus-stack-prometheus-db-prometheus-kube-prometheus-stack-prometheus-0"
+  description = "Used for storage alert. Set if using non-default helm_release"
 }
 
 variable "dependencies" {
