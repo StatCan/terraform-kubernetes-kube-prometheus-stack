7a3f656da2adb5bc9e0b84092c9d267f17fda453
Sajid Hafiz <sajid.hafiz@cloud.statcan.ca>
2023-01-09 13:50:09 -0700

    feat: add runbook links to the prometheus rules

--- alerts.tf
+++ alerts.tf
@@ -11,7 +11,9 @@ resource "kubernetes_manifest" "prometheusrule_certificate_alerts" {
         "rules-definition" = "${local.rules_base_path}/certificate_alerts/certificate_rules.yaml"
       }
     }
-    "spec" = yamldecode(file("${path.module}/prometheus_rules/certificate_alerts/certificate_rules.yaml"))
+    "spec" = yamldecode(templatefile("${path.module}/prometheus_rules/certificate_alerts/certificate_rules.yaml", {
+      runbook_base_url = local.runbook_base_url
+    }))
   }
 }
 
@@ -28,7 +30,9 @@ resource "kubernetes_manifest" "prometheusrule_container_alerts" {
         "rules-definition" = "${local.rules_base_path}/container_alerts/container_rules.yaml"
       }
     }
-    "spec" = yamldecode(file("${path.module}/prometheus_rules/container_alerts/container_rules.yaml"))
+    "spec" = yamldecode(templatefile("${path.module}/prometheus_rules/container_alerts/container_rules.yaml", {
+      runbook_base_url = local.runbook_base_url
+    }))
   }
 }
 
@@ -45,7 +49,9 @@ resource "kubernetes_manifest" "prometheusrule_job_alerts" {
         "rules-definition" = "${local.rules_base_path}/job_alerts/job_rules.yaml"
       }
     }
-    "spec" = yamldecode(file("${path.module}/prometheus_rules/job_alerts/job_rules.yaml"))
+    "spec" = yamldecode(templatefile("${path.module}/prometheus_rules/job_alerts/job_rules.yaml", {
+      runbook_base_url = local.runbook_base_url
+    }))
   }
 }
 
@@ -62,7 +68,9 @@ resource "kubernetes_manifest" "prometheusrule_node_alerts" {
         "rules-definition" = "${local.rules_base_path}/node_alerts/node_rules.yaml"
       }
     }
-    "spec" = yamldecode(file("${path.module}/prometheus_rules/node_alerts/node_rules.yaml"))
+    "spec" = yamldecode(templatefile("${path.module}/prometheus_rules/node_alerts/node_rules.yaml", {
+      runbook_base_url = local.runbook_base_url
+    }))
   }
 }
 
@@ -79,7 +87,9 @@ resource "kubernetes_manifest" "prometheusrule_nodepool_alerts" {
         "rules-definition" = "${local.rules_base_path}/nodepool_alerts/nodepool_rules.yaml"
       }
     }
-    "spec" = yamldecode(file("${path.module}/prometheus_rules/nodepool_alerts/nodepool_rules.yaml"))
+    "spec" = yamldecode(templatefile("${path.module}/prometheus_rules/nodepool_alerts/nodepool_rules.yaml", {
+      runbook_base_url = local.runbook_base_url
+    }))
   }
 }
 
@@ -96,7 +106,9 @@ resource "kubernetes_manifest" "prometheusrule_pod_alerts" {
         "rules-definition" = "${local.rules_base_path}/pod_alerts/pod_rules.yaml"
       }
     }
-    "spec" = yamldecode(file("${path.module}/prometheus_rules/pod_alerts/pod_rules.yaml"))
+    "spec" = yamldecode(templatefile("${path.module}/prometheus_rules/pod_alerts/pod_rules.yaml", {
+      runbook_base_url = local.runbook_base_url
+    }))
   }
 }
 
@@ -115,6 +127,7 @@ resource "kubernetes_manifest" "prometheusrule_prometheus_alerts" {
     }
     "spec" = yamldecode(templatefile("${path.module}/prometheus_rules/prometheus_alerts/prometheus_rules.yaml", {
       prometheus_pvc_name = var.prometheus_pvc_name
+      runbook_base_url    = local.runbook_base_url
     }))
   }
 }
--- locals.tf
+++ locals.tf
@@ -1,8 +1,9 @@
 locals {
   common_labels = {
     "app.kubernetes.io/managed-by" = "terraform"
-    "app.kubernetes.io/version"    = "v3.0.0"
+    "app.kubernetes.io/version"    = "v3.1.0"
     "app.kubernetes.io/part-of"    = "kube-prometheus-stack"
   }
-  rules_base_path = "https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-kubernetes-kube-prometheus-stack/-/tree/master/prometheus_rules"
+  rules_base_path  = "https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-kubernetes-kube-prometheus-stack/-/tree/master/prometheus_rules"
+  runbook_base_url = "https://cloudnative.pages.cloud.statcan.ca/en/documentation/monitoring-surveillance/prometheus"
 }
