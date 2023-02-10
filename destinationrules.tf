714089f4d4e3badee7165fbef014f88e66025f2d
Mohamed Dahrouj <mohamed.dahrouj@cloud.statcan.ca>
2023-02-10 19:27:46 +0000

    feat(alertmanager): add destinationrules for ha

--- destinationrules.tf
+++ destinationrules.tf
@@ -19,6 +19,27 @@ resource "kubernetes_manifest" "destinationrule_kube_prometheus_stack_alertmanag
   }
 }
 
+resource "kubernetes_manifest" "destinationrule_kube_prometheus_stack_alertmanager_replicas" {
+  count = var.enable_destinationrules && var.alertmanager_replicas > 1 ? var.alertmanager_replicas : 0
+  manifest = {
+    "apiVersion" = "networking.istio.io/v1beta1"
+    "kind"       = "DestinationRule"
+    "metadata" = {
+      "labels"    = merge(var.destinationrules_labels, local.common_labels)
+      "name"      = "${var.helm_release}-alertmanager-${count.index}"
+      "namespace" = var.helm_namespace
+    }
+    "spec" = {
+      "host" = "${var.helm_release}-alertmanager-${count.index}.${var.helm_namespace}.svc.${var.cluster_domain}"
+      "trafficPolicy" = {
+        "tls" = {
+          "mode" = var.destinationrules_mode
+        }
+      }
+    }
+  }
+}
+
 resource "kubernetes_manifest" "destinationrule_kube_prometheus_stack_grafana" {
   count = var.enable_destinationrules ? 1 : 0
   manifest = {
